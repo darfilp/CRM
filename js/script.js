755e9ba95f1f4f9d53a34a156984b4828677ba2b
Daria <wwdarcool@yandex.ru>
2022-05-12 17:14:53 +0300

    add lesson2

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const title = document.querySelector('h1');
+const form = document.querySelector('.modal');
+const checkbox = document.querySelector('.modal__checkbox');
+const discont = document.querySelector('.modal');
+
+console.log(title);
+console.log(form);
+console.log(checkbox);
+console.log(discont);
