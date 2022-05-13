45257bbd9cf2ea35e8af5483c4ab0dc2960e26b8
Daria <wwdarcool@yandex.ru>
2022-05-13 12:04:58 +0300

    fix error

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const title = document.querySelector('h1');
-const form = document.querySelector('.modal');
+const title = document.querySelector('.modal__title');
+const form = document.querySelector('.modal__form');
 const checkbox = document.querySelector('.modal__checkbox');
-const discont = document.querySelector('.modal');
+const discont = document.querySelector('.modal__input_discount');
 
 console.log(title);
 console.log(form);
