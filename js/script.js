32488ccaa0759b4407f4b6eb440a9ccc0fa75f95
Daria <wwdarcool@yandex.ru>
2022-05-14 13:37:53 +0300

    add fix bug

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ const title = document.querySelector('.modal__title');
 const form = document.querySelector('.modal__form');
 const checkbox = document.querySelector('.modal__checkbox');
 const discont = document.querySelector('.modal__input_discount');
+const modal = document.querySelector('.overlay');
+const tableBody = document.querySelector('.table__body')
 
 console.log(title);
 console.log(form);
 console.log(checkbox);
 console.log(discont);
+
