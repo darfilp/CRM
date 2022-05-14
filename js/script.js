fced6c30d09612c678b05d7c9a2d8c393bdbdc8e
Daria <wwdarcool@yandex.ru>
2022-05-14 13:38:28 +0300

    add lesson3

--- js/script.js
+++ js/script.js
@@ -10,3 +10,30 @@ console.log(form);
 console.log(checkbox);
 console.log(discont);
 
+//2
+modal.classList.remove('active');
+
+fetch('./goods.json').then(response => response.json())
+  .then(datas => {
+      datas.forEach(data => {
+        let dom = renderGoods(data);
+        tableBody.insertAdjacentHTML('beforeend', dom)
+      });
+  })
+
+const renderGoods = (data) => {
+    return `<tr data-id="${data.id}">
+    <td class="table__cell">${data.id}</td>
+    <td class="table__cell table__cell_left table__cell_name" >${data.title}</td>
+    <td class="table__cell table__cell_left">${data.category}</td>
+    <td class="table__cell">${data.units}</td>
+    <td class="table__cell">${data.count}</td>
+    <td class="table__cell">$${data.price}</td>
+    <td class="table__cell">$${(data.count * data.price) - (data.price * data.count * data.discont / 100)}</td>
+    <td class="table__cell table__cell_btn-wrapper">          
+      <button class="table__btn table__btn_pic"></button>
+      <button class="table__btn table__btn_edit"></button>
+      <button class="table__btn table__btn_del"></button>
+    </td>
+  </tr> `
+}
